2769d4881e0faa9fac549a69a4a6e0369a0b45f4
silvercen <abelkm123@gmail.com>
2024-10-17 11:57:31 +0530

    Made changes in Userinfo(manual getter and setter instead of lombok)

--- src/main/java/com/ust/Security_Microservice/Model/Userinfo.java
+++ src/main/java/com/ust/Security_Microservice/Model/Userinfo.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Userinfo {
@@ -18,4 +17,32 @@ public class Userinfo {
     private String username;
     private String password;
     private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
 }
